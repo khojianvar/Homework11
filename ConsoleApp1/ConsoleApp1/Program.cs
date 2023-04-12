777ebf3dca5a340cf43101e99d543d18ed68e554
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-04-12 21:45:51 +0500

    Task 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter m: ");
+            int m = int.Parse(Console.ReadLine());
+            int[][] array = new int[n][];
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new int[m];
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = random.Next(-10, 10);
+                    sum += array[i][j];
+                    Console.Write($" {array[i][j]}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Amount of array`s elements: {n*m}");
+            Console.WriteLine($"Sum of array`s elements: {sum}");
+            if(sum>n*m)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
         }
     }
 }
