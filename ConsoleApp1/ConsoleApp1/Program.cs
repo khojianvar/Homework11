eae3e4203cec78233565d62bdd357401f295b92d
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-04-13 23:32:52 +0500

    Task7

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your sentence: ");
+            string str = Console.ReadLine();
+            StrSoz(str);
+        }
+        public static void StrSoz(string str)
+        {
+
+            string sentence = str.ToLower();
+            string[] strings = sentence.Split(' ');
+            for (int i = 0; i < strings.Length; i++)
+            {
+                strings[i] = strings[i].Substring(0, 1).ToUpper() + strings[i].Substring(1);
+
+                Console.Write($"{strings[i]} ");
+            }
         }
     }
 }
