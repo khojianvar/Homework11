6b9d258da24d725191e42f96217289f7006e911e
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-04-13 23:16:37 +0500

    Task6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random= new Random();
+            Console.Write("Enter the length of array: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+            int countPositive = 0;
+            int countNegative = 0;
+            int[] arrayPositive = new int[length];
+            int[] arrayNegative = new int[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-10, 11);
+                if (array[i] >= 0)
+                {
+                    arrayPositive[countPositive] = array[i];
+                    countPositive++;
+                }
+                else
+                {
+                    arrayNegative[countNegative] = array[i];
+                    countNegative++;
+                }
+                Console.Write($" {array[i]}");
+            }
+            Console.WriteLine();
+            for (int i = 0; i< countPositive; i++)
+            {
+                Console.Write($" {arrayPositive[i]}");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < countNegative; i++)
+            {
+                Console.Write($" {arrayNegative[i]}");
+            }
         }
     }
 }
