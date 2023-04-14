7005b0d1d89ee1bdda83a41bccd6fcdf04bf9492
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-04-14 09:05:02 +0500

    Updated code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,22 +8,15 @@
             string str1 = Console.ReadLine();
             Console.Write("Enter word what do you search: ");
             string str2 = Console.ReadLine();
-            int counter = 0;
-            for (int i = 0; i < str1.Length; i++)
-            {
-                if (str1.Substring(i, str2.Length) == str2)
-                {
-                    counter++;
-                }
-            }
-            Console.WriteLine($"Result: {counter}");
+            int res = AmountOfYYY(str1,str2);
+            Console.WriteLine($"Output: {res}");
         }
         public static int AmountOfYYY(string str1, string str2)
         {
             int counter = 0;
             for (int i = 0; i < str1.Length; i++)
             {
-                if (str1.Substring(i, str2.Length) == str2)
+                if ((i < str1.Length-2) && (str1.Substring(i, str2.Length) == str2))
                 {
                     counter++;
                 }
